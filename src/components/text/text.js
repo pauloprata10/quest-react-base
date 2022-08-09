09a8f1a9583e92c11cfe153f00cb9b9a663c2a9d
Paulo Prata <pauloprata10@gmail.com>
2022-08-09 00:51:07 -0300

    aplicando correções

--- src/components/text/text.js
+++ src/components/text/text.js
@@ -1,21 +1,7 @@
 import React from 'react'
-import './text.css'
 
-class Text extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>{this.props.title}</h1>
-        <p>
-          O React é uma biblioteca JavaScript de código aberto com foco em criar
-          interfaces de usuário em páginas web. É mantido pelo Facebook,
-          Instagram, outras empresas e uma comunidade de desenvolvedores
-          individuais. É utilizado nos sites da Netflix, Imgur, Feedly, Airbnb,
-          SeatGeek, HelloSign, Walmart e outros.
-        </p>
-      </div>
-    )
-  }
+const Text = ({ texto }) => {
+  return <p style={{color: 'red'}}>{texto.toUpperCase()}</p>
 }
 
-export default Text
+export { Text }
